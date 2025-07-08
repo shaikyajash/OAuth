09e19d59d4891a57fbaed5ab2a0e4903031a2dba
shaikyajash <shaikyajash@gmail.com>
2025-07-08 19:19:10 +0530

    updating  cross-site cookies

--- backend/utils/Reuseable.js
+++ backend/utils/Reuseable.js
@@ -19,7 +19,7 @@ const sendCookie = (res, name, value, statusCode = 200, expiryMinutes = 30) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       
-      sameSite: isProd ? 'None' : 'Lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
 
 
       expires: new Date(Date.now() + expiryMinutes * 60 * 1000), // Default to 30 minutes
